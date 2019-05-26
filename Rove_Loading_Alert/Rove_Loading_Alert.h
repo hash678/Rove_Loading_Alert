f04e059a809f9fef31e14548d97397c7592d6aa5
Hassan Abbasi <hassananjum331@gmail.com>
2019-05-26 15:17:59 +0500

    Initial Commit

---
+++ Rove_Loading_Alert/Rove_Loading_Alert.h
@@ -0,0 +1,19 @@
+//
+//  Rove_Loading_Alert.h
+//  Rove_Loading_Alert
+//
+//  Created by Hassan Abbasi on 26/05/2019.
+//  Copyright © 2019 Rove. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Rove_Loading_Alert.
+FOUNDATION_EXPORT double Rove_Loading_AlertVersionNumber;
+
+//! Project version string for Rove_Loading_Alert.
+FOUNDATION_EXPORT const unsigned char Rove_Loading_AlertVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Rove_Loading_Alert/PublicHeader.h>
+
+
